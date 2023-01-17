21b24f3e05779071b2676b2abfba0299b8dfee15
Himanshu Suryawanshi <97045316+himanshusuryawanshi@users.noreply.github.com>
2023-01-17 21:25:30 +0530

    Update settings.py

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
